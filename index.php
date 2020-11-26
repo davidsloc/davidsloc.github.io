33f472c234bdfa7f2da1cc22e0ce0027fa9257a1
davidsloc <davidsloc00@gmail.com>
2020-11-26 20:20:12 +0200

    Add index.php css and js

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Title</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script type="text/javascript" src="js/script.js" defer></script>
+    </head>
+    <body>
+        <h1>Hello Poppers</h1>
+    </body>
+</html>
