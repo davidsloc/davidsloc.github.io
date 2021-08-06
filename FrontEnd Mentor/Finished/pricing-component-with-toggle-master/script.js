4b1725a7b674fe0a0c2ecacdc4992067f526100d
Dāvids Locāns <davidsloc00@gmail.com>
2021-08-06 22:09:13 +0300

    creating new

--- FrontEnd Mentor/Finished/pricing-component-with-toggle-master/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('toggle');
-const flexy = document.getElementById('flexy');
-
-toggle.addEventListener('change',(e) =>{
-    flexy.classList.toggle('show-monthly');
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-# davidsloc.github.io
